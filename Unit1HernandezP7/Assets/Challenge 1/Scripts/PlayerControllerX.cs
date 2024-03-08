8d6770520ef9bb1ec97ea965d83aa0d3319d49c1
Byron Hernandez <739744@student.dusd.net>
2024-03-08 15:27:37 -0800

    Barely did anything
    Only did the camera

--- Unit1HernandezP7/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Unit1HernandezP7/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5.0f;
     public float rotationSpeed;
     public float verticalInput;
+    private float horizontalInput;
+    private float turnSpeed = 25.0f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +20,18 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+
+
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
--- Unit1HernandezP7/Assets/Scripts/PlayerController.cs
+++ Unit1HernandezP7/Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.forward * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         if(Input.GetKeyDown(switchKey))
